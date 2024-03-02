f18936763e06b1657979385b3f8bbb36e667f288
= <=>
2024-03-01 23:41:41 -0500

    feat: added agent attributes

--- src/agents/base/agent_base.rs
+++ src/agents/base/agent_base.rs
@@ -1,3 +1,7 @@
+use crate::models::general::llm::Message;
+
+use super::agent_traits::BasicAgentTraits;
+
 #[derive(Debug, PartialEq)]
 pub enum AgentState {
     Discovery,
@@ -6,7 +10,49 @@ pub enum AgentState {
     Finished
 }
 
-pub struct BaseAgent {
-    objective: String,
-    position: String
+#[derive(Debug)]
+pub struct AgentAttributes {
+    pub objective: String,
+    pub position: String,
+    pub state: AgentState,
+    pub memory: Vec<Message>
+}
+
+impl BasicAgentTraits for AgentAttributes {
+    fn new(objective: String, position: String) -> Self {
+        Self {
+            objective,
+            position,
+            state: AgentState::Discovery,
+            memory: Vec::new()
+        }
+    }
+
+    fn print_agent_attributes(&self) {
+        println!("{:?}", self);
+    }
+
+    fn print_position(&self) {
+        println!("Current agent has position {}", self.position);
+    }
+
+    fn update_agent_state(&mut self, new_state: AgentState) {
+        self.state = new_state;
+    }
+
+    fn get_agent_state(&self) -> &AgentState {
+        &self.state
+    }
+
+    fn get_agent_position(&self) -> &String {
+        &self.position
+    }
+
+    fn get_agent_objective(&self) -> &String {
+        &self.objective
+    }
+
+    fn get_agent_memory(&self) -> &Vec<Message> {
+        &self.memory
+    }
 }
--- src/agents/base/agent_traits.rs
+++ src/agents/base/agent_traits.rs
@@ -1,8 +1,12 @@
-use crate::agents::base::agent_base::AgentState;
+use crate::{agents::base::agent_base::AgentState, models::general::llm::Message};
 
 pub trait BasicAgentTraits {
     fn new(objective: String, position: String) -> Self;
-    fn print_object(&self) -> String;
-    fn print_position(&self) -> String;
+    fn print_agent_attributes(&self);
+    fn print_position(&self);
     fn update_agent_state(&mut self, new_state: AgentState);
+    fn get_agent_state(&self) -> &AgentState;
+    fn get_agent_position(&self) -> &String;
+    fn get_agent_objective(&self) -> &String;
+    fn get_agent_memory(&self) -> &Vec<Message>;
 }
