a2757e2e703b9f3301db5d1833831efb91bf551d
= <=>
2024-03-09 20:00:51 -0500

    added printing different types of messages capabilities

--- src/utils/command_line.rs
+++ src/utils/command_line.rs
@@ -4,6 +4,45 @@ use crossterm::{
 };
 use std::io::{stdin, stdout};
 
+#[derive(PartialEq, Debug)]
+pub enum PrintMessage {
+    Info,
+    Testing,
+    Error
+}
+
+impl PrintMessage {
+    pub fn print_agent_msg(&self, agent_pos: &str, agent_statement: &str) {
+        let mut stdout: std::io::Stdout = stdout();
+        let msg_color: Color = match self {
+            Self::Info => Color::Rgb { // blue color
+                r: 61,
+                g : 103,
+                b: 242
+            },
+            Self::Testing => Color::Rgb { // Yellow color
+                r: 219,
+                g : 206,
+                b: 88
+            },
+            Self::Error => Color::Rgb { // Red color
+                r: 219,
+                g : 88,
+                b: 129
+            }
+        };
+
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("Agent: {} - ", agent_pos);
+
+        stdout.execute(SetForegroundColor(msg_color)).unwrap();
+        println!("{}", agent_statement);
+
+        // Reset text color back
+        stdout.execute(ResetColor).unwrap();
+    }
+}
+
 pub fn get_user_input(question: &str, question_num: u8) -> String {
     let mut stdout: std::io::Stdout = stdout();
     // Prompt
@@ -47,4 +86,23 @@ pub fn get_user_input(question: &str, question_num: u8) -> String {
     );
 
     user_input
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn tests_command_line_messages() {
+        
+        // Info
+        PrintMessage::Info.print_agent_msg("Project Manager", "Converting user input to goal");
+
+        // Testing
+        PrintMessage::Testing.print_agent_msg("Backend engineer", "Testing backend code");
+
+        // Error
+        PrintMessage::Error.print_agent_msg("Solutions Architect", "Testing Error");
+    }
 }
